9625405b4197db5964aecf83c31c53f46c1abb55
ahmed <ahmedhalwgy@gmail.com>
2024-02-07 06:00:06 +0200

    Auth added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiii');
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieSession({keys:['dsggs']}))
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+app.listen(8080, () => {
+  console.log('App running on port 8080');
+});
